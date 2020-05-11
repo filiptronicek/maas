bc77a0ec90ec6635726949cce0684f40b650b005
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-11 15:36:48 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'lmao yeet';
+$url = file_get_contents('https://meme-api.herokuapp.com/gimme');
+$url = json_decode($url);
+$url = $url->url;
+
+$data = file_get_contents($url);
+
+$ext = explode(".", $url);
+$ext = end($ext);
+header("Content-type: image/" . $ext);
+echo $data;
